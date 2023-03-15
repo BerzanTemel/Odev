458ebf1a6d318f0056ba34567d9b8e2288c178e8
BerzanTemel <127979097+BerzanTemel@users.noreply.github.com>
2023-03-15 21:15:59 +0300

    Berzan_Temel_1220505037

---
+++ soru1/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char *argv[]) {
+	
+	
+	/*
+	
+	soru 1in cevabýdýr;
+	
+	Algoritmayý neden analiz ederiz?
+
+Algoritma analizi, bir algoritmanýn iþlem süresi, bellek kullanýmý ve genel performansýný deðerlendirmek için yapýlan bir analiz türüdür. Algoritma analizi, bir algoritmanýn verimliliðini ve performansýný optimize etmek için kullanýlýr. Bunun nedeni, bir algoritmanýn verimliliðinin artmasý, uygulama veya sistemin çalýþma süresinin kýsalmasý ve daha az bellek kullanýmý gerektirmesi anlamýna gelir.
+Algoritma analizi ayný zamanda, farklý algoritmalarýn performansýný karþýlaþtýrmak için de kullanýlabilir. Örneðin, ayný iþlevi gerçekleþtiren farklý algoritmalarýn performansýný karþýlaþtýrmak, hangi algoritmanýn en verimli sonucu üreteceðini belirlemek için yapýlabilir.
+Bu nedenlerle, algoritma analizi, bir algoritmanýn optimize edilmesi, performansýnýn artýrýlmasý ve verimliliðinin iyileþtirilmesi için önemli bir araçtýr.
+
+
+
+
+
+Algoritma analizi nasýl yapýlýr ?
+Algoritma analizi genellikle zaman ve bellek kullanýmý açýsýndan yapýlýr. Algoritmanýn hangi durumlarda ne kadar sürede ve ne kadar bellek kullanacaðý tahmin edilir. Bu tahminler, algoritmanýn optimize edilmesi ve daha iyi bir performans elde edilmesi için kullanýlýr.
+
+Algoritma analizi yaparken þu adýmlar izlenebilir:
+
+
+
+1-Algoritmanýn çalýþma prensibini anlayýn ve adýmlarýný belirleyin.
+2-Algoritmanýn en iyi ve en kötü durumlarda (en az ve en çok iþlem gerektiren durumlar) ne kadar zaman ve bellek kullanacaðýný tahmin edin.
+3-Analiz yöntemlerinden birini seçin: Büyük O, Omega veya Theta gibi.
+4-Algoritmanýn iþlem süresi ve bellek kullanýmýný tahmin edin.
+5-Tahminlerinizi test edin ve doðrulayýn. Algoritmayý gerçek bir veri seti üzerinde çalýþtýrarak performansýný ölçün ve tahminlerinizle karþýlaþtýrýn.
+6-Algoritmayý optimize etmek için gerekli düzenlemeleri yapýn.
+
+Algoritma analizi yapmak, bir algoritmanýn etkinliðini artýrmak ve performansýný optimize etmek için önemlidir. Ancak algoritma analizi karmaþýk olabilir ve zaman alabilir. Bu nedenle, algoritma analizinde deneyimli olmayanlar genellikle bir uzmana baþvurarak yardým alabilirler.
+
+	
+	
+	return 0;
+}
